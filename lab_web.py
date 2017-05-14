4681547297c311be138d05030e498561e9b4a1d4
Collin May <ziggit@gmail.com>
2017-05-14 01:51:06 -0500

    Add yaml configuration option

--- lab_web.py
+++ lab_web.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import render_template
 import requests
+import yaml
+
 app = Flask(__name__)
 
 @app.context_processor
@@ -13,26 +15,25 @@ def utility_processor():
             return "Down"
     return dict(state_to_friendly=state_to_friendly)
 
+def read_conf(conf='conf.yaml'):
+    with open(conf) as f:
+        conf_items = yaml.load(f)
+    return conf_items
+
 @app.route('/')
 def labs():
-    r = requests.get('http://172.19.17.6/env.json')
-    r2 = requests.get('http://172.19.17.7/env.json')
-    sources = {'suplab01':'http://172.19.17.6/env.json',
-               'suplab02':'http://172.19.17.7/env.json',}
-    {'nopelab01': 'localhost',
-     'nopelab02': 'localhost'}
-
     all_envs = []
     unavailable = []
-    for platform, url in sources.items():
+    sources = [x for x in read_conf()['platforms'] if x['platform_type'] == 'fuel-devops-http' and x['enabled'] == True]
+    for platform in sources:
         try:
-            req = requests.get(url)
+            req = requests.get(platform['url'])
             if req.status_code == 200:
                 platform_req = req.json()['environments']
-                [n.update({'platform': platform}) for e in platform_req for n in e]        
+                [n.update({'platform': platform['name']}) for e in platform_req for n in e]        
                 all_envs = all_envs + platform_req
         except:
-            unavailable.append(platform)
+            unavailable.append(platform['name'])
 
     # req_envs = [x for y in r.json()['environments']+r2.json()['environments'] for x in y if x['node'] == 'admin']
     req_envs = [x for y in all_envs for x in y if x['node'] == 'admin']
